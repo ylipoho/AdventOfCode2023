02b8a1d26fb39f73671ac18a94cc6fb7eb7adfed
Baranouskaya Lizaveta <elizaveta.baranovskaya.21@gmail.com>
2024-11-04 21:42:29 +0300

    Upload solutions for 5.1, 15.1

---
+++ src/Day15.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+
+namespace AdventOfCode2023.src
+{
+	public static class Day15
+	{
+		public static int Get_v1()
+		{
+			string fileName = @"..\..\..\resources\Day15-Input.txt";
+			List<string> lines = new List<string>();
+
+			if (File.Exists(fileName))
+			{
+				lines = File.ReadAllLines(fileName).ToList();
+			}
+
+			string[] substrings = lines[0].Split(',');
+			List<int> currentValues = new List<int>();
+
+			foreach (var substring in substrings)
+			{
+				byte[] bytes = Encoding.ASCII.GetBytes(substring);
+				int currentValue = 0;
+
+				foreach (var b in bytes)
+				{
+					currentValue = (currentValue + b) * 17 % 256;
+				}
+				currentValues.Add(currentValue);
+			}
+
+			return currentValues.Sum();
+		}
+	}
+}
---
+++ src/Day5.cs
@@ -0,0 +1,81 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode2023.src
+{
+	internal class Day5
+	{
+		public static long GetLowestLocation_v1()
+		{
+			string fileName = @"..\..\..\resources\Day5-Input.txt";
+			string[] lines = new string[0];
+
+			if (File.Exists(fileName))
+			{
+				lines = File.ReadAllLines(fileName);
+			}
+
+			// seeds numbers
+			long[] seeds = Regex.Matches(lines[0], @"\d+")
+											.Select(s => long.Parse(s.Value))
+											.ToArray();
+
+			// map line indexes
+			int[] mapHeaderLines = lines.Select((l, index) => new { l, index })
+									.Where(obj => obj.l.Contains("map:"))
+									.Select(obj => obj.index + 1)
+									.Append(lines.Length + 1)
+									.ToArray();
+			
+			// index of first line with 3 numbers
+			int firstDataLineIndex = 3;
+
+			// ПРОХОД ПО КАЖДОЙ МАПЕ
+			for (int i = 0; i < mapHeaderLines.Count() - 1; i++)
+			{
+				List<long[]> map = new List<long[]>();
+
+				// на каждом проходе заполнить 1 карту 
+				while (firstDataLineIndex < lines.Count() && lines[firstDataLineIndex].Any(c => char.IsDigit(c)))
+				{
+					long[] threeValue = Regex.Matches(lines[firstDataLineIndex], @"\d+")
+									.Select(v => long.Parse(v.Value))
+									.ToArray();
+					map.Add(threeValue);
+					firstDataLineIndex++;
+				}
+
+				firstDataLineIndex += 2;
+
+				// для каждой семечки
+				for (int j = 0; j < seeds.Count(); j++)
+				{
+					long currentSeedParameter = -1;
+					// пройтись по всем строкам 1ой мапы
+					for (int k = 0; k < map.Count; k++)
+					{
+						long diff = seeds[j] - map[k][1];
+						if (diff >0 && diff <= map[k][2])
+						{
+							currentSeedParameter = map[k][0] + diff;
+							break;
+						}
+					}
+
+					seeds[j] = currentSeedParameter < 0 ? seeds[j] : currentSeedParameter;
+					Console.WriteLine("seeds[" + j+ "]= " + seeds[j]);
+					// нашли location
+				}
+
+				int a = 0;
+			}
+
+			foreach (var r in seeds)
+			{
+				Console.WriteLine("r = "+r);
+			}
+
+			// 157211394
+			return seeds.Min();
+		}
+	}
+}
